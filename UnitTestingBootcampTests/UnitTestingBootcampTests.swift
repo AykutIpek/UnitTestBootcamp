f05a710c704a06f1e1ffa6758dfabd2c3229cecc
AykutIpek <aykutipek_2001@hotmail.com>
2023-07-05 17:03:37 +0300

    test_UnitTestingBootcampViewModel_isPremium_shouldBeInjectedValue func is tessted

--- UnitTestingBootcampTests/UnitTestingBootcampTests.swift
+++ UnitTestingBootcampTests/UnitTestingBootcampTests.swift
@@ -9,28 +9,42 @@ import XCTest
 @testable import UnitTestingBootcamp
 
 final class UnitTestingBootcampTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_UnitTestingBootcampViewModel_isPremium_shouldBeTrue(){
+        // Given
+        let userIsPremium: Bool = true
+        // When
+        let vm = UnitTestingBootcampViewModel(isPremium: userIsPremium)
+        // Then
+        XCTAssertTrue(vm.isPremium)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_UnitTestingBootcampViewModel_isPremium_shouldBeFalse(){
+        // Given
+        let userIsPremium: Bool = false
+        // When
+        let vm = UnitTestingBootcampViewModel(isPremium: userIsPremium)
+        // Then
+        XCTAssertFalse(vm.isPremium)
     }
-
+    
+    func test_UnitTestingBootcampViewModel_isPremium_shouldBeInjectedValue(){
+        // Given
+        let userIsPremium: Bool = Bool.random()
+        // When
+        let vm = UnitTestingBootcampViewModel(isPremium: userIsPremium)
+        // Then
+        XCTAssertEqual(vm.isPremium, userIsPremium)
+    }
+    
+    
+    
 }
