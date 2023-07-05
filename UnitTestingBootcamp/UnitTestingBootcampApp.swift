59bbeae424163cd1784d80daef691cda57f8aee5
AykutIpek <aykutipek_2001@hotmail.com>
2023-07-05 15:45:50 +0300

    Initial Commit

---
+++ UnitTestingBootcamp/UnitTestingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingBootcampApp.swift
+//  UnitTestingBootcamp
+//
+//  Created by aykut ipek on 5.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
