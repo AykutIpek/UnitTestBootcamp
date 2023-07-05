126da004831a207c7e7751c32a8b870cb30c9738
AykutIpek <aykutipek_2001@hotmail.com>
2023-07-05 17:40:11 +0300

    Selected Items Func added in Unit Test File

--- UnitTestingBootcampTests/UnitTestingBootcampTests.swift
+++ UnitTestingBootcampTests/UnitTestingBootcampTests.swift
@@ -45,6 +45,108 @@ final class UnitTestingBootcampTests: XCTestCase {
         XCTAssertEqual(vm.isPremium, userIsPremium)
     }
     
+    func test_UnitTestingBootcampViewModel_isPremium_shouldBeInjectedValue_stress(){
+        for _ in 0..<100 {
+            // Given
+            let userIsPremium: Bool = Bool.random()
+            // When
+            let vm = UnitTestingBootcampViewModel(isPremium: userIsPremium)
+            // Then
+            XCTAssertEqual(vm.isPremium, userIsPremium)
+        }
+    }
+    
+    func test_UnitTestingBootcampViewModel_dataArray_shouldBeEmpty(){
+        // Given
+        
+        // When
+        let vm = UnitTestingBootcampViewModel(isPremium: Bool.random())
+        // Then
+        XCTAssertEqual(vm.dataArray.count, 0)
+        
+        
+    }
+    
+    func test_UnitTestingBootcampViewModel_dataArray_shouldAddItems(){
+        // Given
+        let vm = UnitTestingBootcampViewModel(isPremium: Bool.random())
+        // When
+        let loopCount: Int = Int.random(in: 1..<100)
+        for _ in 0..<loopCount {
+            vm.addItem(item: UUID().uuidString)
+        }
+        // Then
+        XCTAssertTrue(!vm.dataArray.isEmpty)
+        XCTAssertFalse(vm.dataArray.isEmpty)
+        XCTAssertEqual(vm.dataArray.count, loopCount)
+        XCTAssertNotEqual(vm.dataArray.count, 0)
+        XCTAssertGreaterThan(vm.dataArray.count, 0)
+        
+    }
+    
+    func test_UnitTestingBootcampViewModel_dataArray_shouldNotAddBlankString(){
+        // Given
+        let vm = UnitTestingBootcampViewModel(isPremium: Bool.random())
+        // When
+        vm.addItem(item: "")
+        // Then
+        XCTAssertTrue(vm.dataArray.isEmpty)
+    }
+    
+    func test_UnitTestingBootcampViewModel_selectedItem_shouldStartAsNil(){
+        // Given
+        
+        // When
+        let vm = UnitTestingBootcampViewModel(isPremium: Bool.random())
+        // Then
+        XCTAssertTrue(vm.selectedItem == nil)
+        XCTAssertNil(vm.selectedItem)
+        
+    }
+    
+    func test_UnitTestingBootcampViewModel_selectedItem_shouldBeNilWhenSelectingInvalidItem(){
+        // Given
+        let vm = UnitTestingBootcampViewModel(isPremium: Bool.random())
+        // When
+        /// Select valid item
+        let newItem = UUID().uuidString
+        vm.addItem(item: newItem)
+        vm.selectedItem(item: newItem)
+        /// Select invalid item
+        vm.selectedItem(item: UUID().uuidString)
+        // Then
+        XCTAssertNil(vm.selectedItem)
+    }
+    
+    func test_UnitTestingBootcampViewModel_selectedItem_shouldBeSelected(){
+        // Given
+        let vm = UnitTestingBootcampViewModel(isPremium: Bool.random())
+        // When
+        let newItem = UUID().uuidString
+        vm.addItem(item: newItem)
+        vm.selectedItem(item: newItem)
+        // Then
+        XCTAssertNotNil(vm.selectedItem)
+        XCTAssertEqual(vm.selectedItem, newItem)
+    }
+    
+    func test_UnitTestingBootcampViewModel_selectedItem_shouldBeSelected_stress(){
+        // Given
+        let vm = UnitTestingBootcampViewModel(isPremium: Bool.random())
+        // When
+        let loopCount: Int = Int.random(in: 1..<100)
+        var itemsArray: [String] = []
+        for _ in 0..<loopCount{
+            let newItem = UUID().uuidString
+            vm.addItem(item: newItem)
+            itemsArray.append(newItem)
+        }
+        let randomItem = itemsArray.randomElement() ?? ""
+        vm.selectedItem(item: randomItem)
+        // Then
+        XCTAssertNotNil(vm.selectedItem)
+        XCTAssertEqual(vm.selectedItem, randomItem)
+    }
     
     
 }
